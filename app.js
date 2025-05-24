fe09a1eff343f4cef65e009d4539f98cf1ac9262
Denis <ldenchik215@gmail.com>
2025-05-24 21:59:04 +0700

    edit

---
+++ app.js
@@ -0,0 +1,3 @@
+curl --request POST \
+  --url 'https://api.vk.com/method/wall.post?owner_id=-26991292&message=test%20message&v=5.131' \
+  --header 'Authorization: Bearer vk1.a.1kD3xZjypCdCX6ocaxfJD5NLO2dj8iv1qRCyYpKv0SYw8JVNX8-GSUz1Fski_n5P8cK1fDp_8tvK7bbYln7pbOAlPHs_wI45mVgTqyDn6Wr1Otbt5WokD_xtkQy3MMKDJYgJpldXBWwdPPX43yeSIzYCiYFrmW3NPZGeGQRTqKdU8B2k0FzPy-vY4QAthdeAbWGV6XLHllCe9Kn48PdgeQ'
